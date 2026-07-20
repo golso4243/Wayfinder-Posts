cf96d9a33b8262f3b79e81c99dd484deae362af2
SwornHero <92185460+golso4243@users.noreply.github.com>
2026-07-20 17:02:59 -0500

    feat: add horizontal facing to wayfinder post

--- src/main/java/com/swornhero/wayfinderposts/block/WayfinderPostBlock.java
+++ src/main/java/com/swornhero/wayfinderposts/block/WayfinderPostBlock.java
@@ -1,18 +1,61 @@
 package com.swornhero.wayfinderposts.block;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
+import net.minecraft.world.level.block.Mirror;
+import net.minecraft.world.level.block.Rotation;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.EnumProperty;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class WayfinderPostBlock extends Block {
+    public static final EnumProperty<Direction> FACING =
+            HorizontalDirectionalBlock.FACING;
+
     private static final VoxelShape POST_SHAPE =
             Block.box(6.0, 0.0, 6.0, 10.0, 16.0, 10.0);
 
     public WayfinderPostBlock(Properties properties) {
         super(properties);
+
+        registerDefaultState(
+                stateDefinition.any()
+                        .setValue(FACING, Direction.NORTH)
+        );
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        return defaultBlockState()
+                .setValue(FACING, context.getHorizontalDirection().getOpposite());
+    }
+
+    @Override
+    protected void createBlockStateDefinition(
+            StateDefinition.Builder<Block, BlockState> builder
+    ) {
+        builder.add(FACING);
+    }
+
+    @Override
+    protected BlockState rotate(BlockState state, Rotation rotation) {
+        return state.setValue(
+                FACING,
+                rotation.rotate(state.getValue(FACING))
+        );
+    }
+
+    @Override
+    protected BlockState mirror(BlockState state, Mirror mirror) {
+        return state.rotate(
+                mirror.getRotation(state.getValue(FACING))
+        );
     }
 
     @Override
---
+++ src/main/java/com/swornhero/wayfinderposts/block/package-info.java
@@ -0,0 +1,4 @@
+@NullMarked
+package com.swornhero.wayfinderposts.block;
+
+import org.jspecify.annotations.NullMarked;
